0fc1550c7704814e57da145337b0e766ede90bc5
SergioDev <senorpotato@gmail.com>
2025-12-29 08:14:08 +0100

    Fix CORS for sergiodatos.com

--- main.py
+++ main.py
@@ -17,8 +17,11 @@ def root():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5500", "http://127.0.0.1:5500"],
-    allow_credentials=True,
+    allow_origins=[
+        "https://sergiodatos.com",
+        "https://www.sergiodatos.com",
+    ],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
